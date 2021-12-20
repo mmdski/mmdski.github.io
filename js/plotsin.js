f0050489b9b0bd44081df8fbc0860ea55cb7f7bb
Marian Domanski <mmdski@gmail.com>
2021-12-19 21:42:00 -0600

    use strict and let

--- js/plotsin.js
+++ js/plotsin.js
@@ -1,24 +1,26 @@
 const Plotly = window.Plotly
 
 function plotsin() {
-    var t = [];
-    var x = [];
-    var max = 2 * Math.PI;
-    var nPoints = 100;
+    "use strict";
+    let t = [];
+    let x = [];
+    let max = 2 * Math.PI;
+    let nPoints = 100;
 
-    for (var i = 0; i <= nPoints; i++) {
+    for (let i = 0; i <= nPoints; i++) {
         t.push(i / nPoints * max);
         x.push(Module._chl_sin(t[i]));
     }
 
-    TESTER = document.getElementById('tester');
+    let TESTER = document.getElementById('tester');
 
     staticline(TESTER, t, x);
 }
 
 function staticline(graphDiv, x, y) {
-    var trace = { x: x, y: y, type: 'line' };
-    var layout = {};
-    var config = { staticPlot: true };
+    "use strict";
+    let trace = { x: x, y: y, type: 'line' };
+    let layout = {};
+    let config = { staticPlot: true };
     Plotly.newPlot(graphDiv, [trace], layout, config);
 }
