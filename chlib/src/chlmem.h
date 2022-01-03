fc75e8cecd54b57638914592059000791c4d5581
Marian Domanski <mmdski@gmail.com>
2022-01-03 11:21:57 -0600

    remove chlib project files

--- chlib/src/chlmem.h
+++
@@ -1,9 +0,0 @@
-extern void *chl_alloc (long nbytes);
-
-extern void *chl_calloc (long count, long nbytes);
-
-extern void chl_free (void *ptr);
-
-#define ALLOC(nbytes) chl_alloc ((nbytes))
-#define NEW(p) ((p) = ALLOC ((long) sizeof *(p)))
-#define FREE(ptr) ((void) (chl_free ((ptr)), (ptr) = 0))
